c990d63457fbf034774ae575e39544885c7aa563
Mikhail Bondarev <mikhail.bondarev@onetwotrip.com>
2021-12-22 20:34:47 +0300

    working

--- lib/dumbo-octopus.ex
+++ lib/dumbo-octopus.ex
@@ -1,6 +1,6 @@
 defmodule Octopus do
-
   def getNeighbours(0, 0), do: []
+
   def getNeighbours(i, j) do
     top = to_string(i - 1) <> to_string(j)
     topleft = to_string(i - 1) <> to_string(j - 1)
@@ -15,7 +15,7 @@ defmodule Octopus do
 
   def start({value, {i, j}}) do
     neighbours = getNeighbours(i, j)
-    spawn(fn -> loop({value, neighbours, 0, {i, j}}) end)
+    spawn(fn -> loop({value, neighbours, 0, {i, j}, :false}) end)
   end
 
   def value(server_pid) do
@@ -29,34 +29,50 @@ defmodule Octopus do
 
   def add(server_pid, storePIDs), do: send(server_pid, {:add, storePIDs})
 
-  defp loop({current_value, neighbours, counter, coords}) do
+  def reload(server_pid), do: send(server_pid, {:reload, self()})
+
+  defp loop({current_value, neighbours, counter, coords, lights?}) do
     new_value =
       receive do
         {:value, caller} ->
           send(caller, {:response, {current_value, counter, coords}})
-          {current_value, neighbours, counter, coords}
+          {current_value, neighbours, counter, coords, lights?}
 
         {:add, storePIDs} ->
-          addEnergy(current_value, neighbours, counter, coords, storePIDs) # return???
+          # return???
+          addEnergy(current_value, neighbours, counter, coords, lights?, storePIDs)
+
+        {:reload, _} -> checkState({current_value, neighbours, counter, coords, lights?})
       end
 
     loop(new_value)
   end
 
-  def addEnergy(current_value, neighbours, counter, coords, storePIDs) when current_value >= 9 do
-    IO.inspect({current_value, neighbours, counter, coords}, label: :addEnergy)
+  def addEnergy(current_value, neighbours, counter, coords, :false, storePIDs) when current_value >= 9 do
     neighbours
     |> Enum.map(fn nb -> Map.get(storePIDs, nb, Map.get(storePIDs, "00")) end)
-    |> IO.inspect(label: :forsend)
-    |> Enum.map(fn pid -> Octopus.add(pid, storePIDs) end) # Is it happend???
-
-    {0, neighbours, counter + 1, coords}
+    |> Enum.map(fn pid -> Octopus.add(pid, storePIDs) end)
+    {0, neighbours, counter + 1, coords, :true}
   end
 
-  def addEnergy(current_value, neighbours, counter, coords, _) do
+  def addEnergy(current_value, neighbours, counter, coords, :true, storePIDs) when current_value >= 9 do
+    neighbours
+    |> Enum.map(fn nb -> Map.get(storePIDs, nb, Map.get(storePIDs, "00")) end)
+    |> Enum.map(fn pid -> Octopus.add(pid, storePIDs) end)
+    {0, neighbours, counter, coords, :true}
+  end
+
+  def addEnergy(current_value, neighbours, counter, coords,  :false, _) do
     IO.inspect({current_value, neighbours, counter, coords}, label: :addEnergySimple)
-    {current_value + 1, neighbours, counter, coords}
+    {current_value + 1, neighbours, counter, coords, :false}
   end
+  def addEnergy(current_value, neighbours, counter, coords,  :true, _) do
+    IO.inspect({current_value, neighbours, counter, coords}, label: :addEnergySimple)
+    {current_value, neighbours, counter, coords, :true}
+  end
+
+  def checkState({current_value, neighbours, counter, coords, :true}), do: {0, neighbours, counter, coords, :false}
+  def checkState({current_value, neighbours, counter, coords, :false}), do: {current_value, neighbours, counter, coords, :false}
 
   def run(path, steps \\ 1) do
     inp =
@@ -81,10 +97,26 @@ defmodule Octopus do
 
     1..steps
     |> Enum.map(fn _ ->
-      storePIDs |> Map.values() |> Enum.map(fn pid -> Octopus.add(pid, storePIDs) end)
+      storePIDs
+      |> Map.values()
+      |> Enum.map(fn pid ->
+        Octopus.add(pid, storePIDs)
+        Process.sleep(10)
+        pid
+      end)
+      |> IO.inspect(label: :check)
+      |> Enum.map(fn pid ->
+        Octopus.reload(pid)
+        Process.sleep(10)
+      end)
     end)
 
-    storePIDs |> Map.values() |>  Enum.map(fn pid -> Octopus.value(pid) end) |> IO.inspect()
+    storePIDs
+    |> Map.values()
+    |> Enum.map(fn pid -> Octopus.value(pid) end)
+    |> Enum.filter(fn {_, _, x} -> x != {0,0} end)
+    |> Enum.map(fn {_, counter, _} -> counter end)
+    |> Enum.sum()
 
     # IO.inspect(storePIDs)
 
@@ -92,4 +124,4 @@ defmodule Octopus do
   end
 end
 
-IO.inspect(Octopus.run("./file15"))
+IO.inspect(Octopus.run("./file15", 100))
